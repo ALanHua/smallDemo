d68524df23d181a8d2fe9a3d09dfedf666008fcd
ALanHua <yhp-2009@qq.com>
2018-11-24 00:07:04 +0800

    工具类代码

--- 03_Architecture/03_Architecture/Helpers/Recorder.swift
+++ 03_Architecture/03_Architecture/Helpers/Recorder.swift
@@ -9,4 +9,70 @@
 import Foundation
 import AVFoundation
 
+final class Recorder:NSObject,AVAudioRecorderDelegate{
+    
+    private var audioRecorder:AVAudioRecorder?
+    private var time: Timer?
+    private var update:(TimeInterval?)->()
+    
+    let url:URL
+    
+    init?(url: URL, update: @escaping (TimeInterval?) -> ()){
+        self.update = update
+        self.url = url
+        super.init()
+       
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode:.default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            AVAudioSession.sharedInstance().requestRecordPermission { (allowed) in
+                if allowed {
+                    self.start(url)
+                }else{
+                    self.update(nil)
+                }
+            }
+            
+        } catch {
+            return nil
+        }
+        
+    }
+    
+    private func start(_ url: URL){
+        let setting:[String:Any] = [
+            AVFormatIDKey : kAudioFormatMPEG4AAC,
+            AVSampleRateKey : 44100.0 as Float,
+            AVNumberOfChannelsKey : 1
+        ]
+        
+        if let recorder = try? AVAudioRecorder(url: url, settings: setting) {
+            recorder.delegate = self
+            audioRecorder = recorder
+            recorder.record()
+            time = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+                self.update(self.audioRecorder?.currentTime)
+            })
+        }else{
+            update(nil)
+        }
+        
+    }
+    
+    func stop() {
+        audioRecorder?.stop()
+        time?.invalidate()
+    }
+    
+    // MARK:AVAudioRecorderDelegate
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            stop()
+        }else{
+            update(nil)
+        }
+    }
+    
+}
 
--- 03_Architecture/03_Architecture/Helpers/Utilities.swift
+++ 03_Architecture/03_Architecture/Helpers/Utilities.swift
@@ -5,4 +5,51 @@
 //  Created by yhp on 2018/8/1.
 //  Copyright © 2018年 none. All rights reserved.
 //
+import Foundation
+import UIKit
 
+private let formatter: DateComponentsFormatter = {
+   
+    let formatter = DateComponentsFormatter()
+    // 设置显示格式为9:31:30
+    formatter.unitsStyle = .positional
+    // 设置0占位符格式 09:31:30
+    formatter.zeroFormattingBehavior = .pad
+    formatter.allowedUnits = [
+        .hour,
+        .minute,
+        .second
+    ]
+    return formatter
+}()
+
+func timeString(_ time:TimeInterval) -> String {
+    return formatter.string(from: time)!
+}
+
+
+extension UIViewController {
+    func modalTextAlert(title:String,accept:String = .ok,
+        cancel:String = .cancel,placeholder:String,
+        callback:@escaping (String?)->()) {
+        
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { $0.placeholder = placeholder}
+        
+        alert.addAction(UIAlertAction(title: title, style: .cancel){ _ in
+            callback(nil)
+        })
+        
+        alert.addAction(UIAlertAction(title: title, style: .default){ _ in
+            callback(alert.textFields?.first?.text)
+        })
+        
+    }
+}
+
+
+fileprivate extension String {
+    static let ok = NSLocalizedString("OK", comment: "")
+    static let cancel = NSLocalizedString("Cancel", comment: "")
+}

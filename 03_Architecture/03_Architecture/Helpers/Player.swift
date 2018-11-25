fd59a41f8c1d995cd961608598aef0ffd7408b05
ALanHua <yhp-2009@qq.com>
2018-11-25 23:18:59 +0800

    播放工具类实现

--- 03_Architecture/03_Architecture/Helpers/Player.swift
+++ 03_Architecture/03_Architecture/Helpers/Player.swift
@@ -9,3 +9,81 @@
 import Foundation
 import AVFoundation
 
+class Player: NSObject,AVAudioPlayerDelegate {
+    private var audioPlayer:AVAudioPlayer
+    private var timer:Timer?
+    private var update:(TimeInterval?)->()
+    
+    init?(url: URL, update:@escaping(TimeInterval?)->()) {
+        do{
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        }catch {
+            return nil
+        }
+        
+        if let player = try? AVAudioPlayer(contentsOf: url) {
+            audioPlayer = player
+            self.update = update
+        }else{
+            return nil
+        }
+        
+        super.init()
+        
+        audioPlayer.delegate = self
+    }
+    
+    deinit {
+        timer?.invalidate()
+    }
+    
+    /// 音频文件的总时长
+    var duration:TimeInterval {
+        return audioPlayer.duration
+    }
+    
+    var isPlaying :Bool {
+        return audioPlayer.isPlaying
+    }
+
+    var isPaused : Bool {
+        return !audioPlayer.isPlaying && audioPlayer.currentTime > 0
+    }
+    
+    func togglePlayer() {
+        if audioPlayer.isPlaying {
+            audioPlayer.pause()
+            timer?.invalidate()
+            timer = nil
+        }else{
+            audioPlayer.play()
+            if let t = timer {
+                t.invalidate()
+            }
+            
+            timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block:
+                { [weak self] _ in
+                    guard let s = self else{
+                        return
+                    }
+                    s.update(s.audioPlayer.currentTime)
+            })
+            
+        }
+    }
+    
+    func setProgress(_ time:TimeInterval){
+        audioPlayer.currentTime = time
+    }
+    
+    
+    
+    //  MARK:AVAudioPlayerDelegate
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        timer?.invalidate()
+        timer = nil
+        update(flag ? audioPlayer.currentTime : nil)
+    }
+    
+}
